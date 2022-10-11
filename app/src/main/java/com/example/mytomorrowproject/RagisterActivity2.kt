e70d6046df2b6c5f56116ce56eb27004e3afccc0
kimdaejin1 <s22040@gsm.hs.kr>
2022-10-11 20:09:57 +0900

    first

---
+++ app/src/main/java/com/example/mytomorrowproject/RagisterActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.mytomorrowproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RagisterActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ragister2)
+    }
+}
