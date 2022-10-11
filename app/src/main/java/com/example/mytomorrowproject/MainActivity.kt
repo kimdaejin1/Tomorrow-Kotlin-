f73ae01b43b10923d4efea45ffffe2ea7b0052a1
kimdaejin1 <s22040@gsm.hs.kr>
2022-10-11 20:30:28 +0900

    first

--- app/src/main/java/com/example/mytomorrowproject/MainActivity.kt
+++ app/src/main/java/com/example/mytomorrowproject/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.example.mytomorrowproject
 
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val TAG: String="MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_LOGIN.setOnClickListener{
+            var id=edit_ID.text.toString()
+            var pw=edit_PASS.text.toString()
+
+            val sharedPreferences=getSharedPreferences("file name", Context.MODE_PRIVATE)
+            val savedId=sharedPreferences.getString("id","")
+            val savePass=sharedPreferences.getString("pass","")
+
+            if(id==savedId&&pw==savePass){
+                dialog("success")
+            }
+            else{
+                dialog("fail")
+            }
+        }
+
+        button_RAGISTER.setOnClickListener {
+            val intent = Intent(this,RagisterActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+    fun dialog(type: String){
+        var dialog= AlertDialog.Builder(this)
+
+        if (type.equals("success")) {
+            dialog.setTitle("로그인 성공")
+            dialog.setMessage("로그인 성공!!")
+        }
+        else if(type.equals("fail")){
+            dialog.setTitle("로그인 실패")
+            dialog.setMessage("아이디와 비밀번호를 확인해주세요")
+        }
+
+        var dialog_listener=object: DialogInterface.OnClickListener{
+            override fun onClick(dialog: DialogInterface?, which: Int){
+                when(which){
+                    DialogInterface.BUTTON_POSITIVE->
+                        Log.d(TAG,"")
+                }
+            }
+        }
+        dialog.setPositiveButton("확인",dialog_listener)
+        dialog.show()
     }
 }
--- app/src/main/java/com/example/mytomorrowproject/RagisterActivity2.kt
+++ app/src/main/java/com/example/mytomorrowproject/RagisterActivity2.kt
@@ -1,11 +1,82 @@
 package com.example.mytomorrowproject
 
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_ragister2.*
 
 class RagisterActivity2 : AppCompatActivity() {
+    val TAG: String = "Register"
+    var isExistBlank = false
+    var isPWSame = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ragister2)
+        button_RAGISTER.setOnClickListener {
+            Log.d(TAG, "회원가입 버튼")
+
+            val id = edit_NewID.toString()
+            val pw = edit_NewPass.toString()
+            val pw_re = check_Pass.toString()
+
+            if (id.isEmpty() || pw.isEmpty() || pw_re.isEmpty()) {
+                isExistBlank = true
+            } else {
+                if (pw == pw_re) {
+                    isPWSame = true
+                }
+            }
+
+            if (!isExistBlank && isPWSame) {
+                Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
+
+                val sharedPreference = getSharedPreferences("file name", Context.MODE_PRIVATE)
+                val editer = sharedPreference.edit()
+                editer.putString("id", id)
+                editer.putString("pw", pw)
+                editer.apply()
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                if(isExistBlank){
+                    dialog("blank")
+                }
+                else if(!isPWSame){
+                    dialog("not same")
+                }
+            }
+
+        }
+    }
+    fun dialog(type: String){
+        val dialog= AlertDialog.Builder(this)
+
+        if (type.equals("blank")){
+            dialog.setTitle("회원가입 실패")
+            dialog.setMessage("입력칸을 모두 입력해주세요")
+        }
+        else if (type.equals("not same")){
+            dialog.setTitle("회원가입 실패")
+            dialog.setMessage("비밀번호가 같은지 다시 한번 확인해주세요")
+        }
+
+        val dialog_listener= object : DialogInterface.OnClickListener {
+            override fun onClick(dialog: DialogInterface?, which: Int){
+                when(which){
+                    DialogInterface.BUTTON_POSITIVE ->
+                        Log.d(TAG,"다이얼로그")
+                }
+            }
+        }
+        dialog.setPositiveButton("확인",dialog_listener)
+        dialog.show()
+
     }
 }
