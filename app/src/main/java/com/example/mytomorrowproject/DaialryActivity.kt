d3763889a1da1a6b5218d4288a619e071cf5e315
kimdaejin1 <s22040@gsm.hs.kr>
2022-10-17 14:17:17 +0900

    first

--- app/src/main/java/com/example/mytomorrowproject/DaialryActivity.kt
+++ app/src/main/java/com/example/mytomorrowproject/DaialryActivity.kt
@@ -7,5 +7,7 @@ class DaialryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daialry)
+
+
     }
 }
